dca8db410d28d43145e4562203cb38fef9e7bb52
ronsbons <ronnilouie@gmail.com>
2019-02-14 18:04:38 -0800

    created models for profile and destinations and required them in index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useNewUrlParser: true});
 
 // module.exports.Campsite = require("./campsite.js.example");
+module.exports.Profile = require('./profile.js');
+module.exports.Destinations = require('./destinations.js');
